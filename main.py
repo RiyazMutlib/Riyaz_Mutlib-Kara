03819dd5196aa9fd465cfa8ff717fb63ff78c39e
RiyazMutlib <127445457+RiyazMutlib@users.noreply.github.com>
2023-03-09 16:31:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ def encoder(digits):
     for i in range(0, len(digits)):
       newdig = str((int(digit[i)) + 3))
       if int(digit[i]) >= 7:
-                             
-      new_digits = 
+            newdig = str((int(digit[i)) + 3 - 10))
+      else:
+            newdig = str((int(digit[i)) + 3))
+      new_digits += newdig
