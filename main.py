5704bc5f0bf4092cf82b9c02888a910b53e94dc0
RiyazMutlib <127445457+RiyazMutlib@users.noreply.github.com>
2023-03-09 16:26:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-def encoder():
-  print('hello')
+def encoder(digits):
+   new_digits = ''
+    for i in range(0, len(digits)):
+      newdig = str((int(digit[i)) + 3))
+      if int(digit[i]) >= 7:
+                             
+      new_digits = 
