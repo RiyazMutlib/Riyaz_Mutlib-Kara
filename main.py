5497ee913174c08fecb3bcc476d5443634892dd3
Riyaz_Mutlib <mutlibriyaz@gmail.com>
2023-03-09 16:33:14 -0500

    commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def encoder(digits):
    new_digits = ''
     for i in range(0, len(digits)):
-      newdig = str((int(digit[i)) + 3))
-      if int(digit[i]) >= 7:
-            newdig = str((int(digit[i)) + 3 - 10))
+      newdig = str((int(digits[i)) + 3))
+      if int(digits[i]) >= 7:
+          newdig = str((int(digits[i)) + 3 - 10))
       else:
-            newdig = str((int(digit[i)) + 3))
+            newdig = str((int(digits[i)) + 3))
       new_digits += newdig
+
+    return newdig
