e2c6755e7e6b8f3526d9912b518376fa041a0e6f
Riyaz_Mutlib <mutlibriyaz@gmail.com>
2023-03-20 15:23:07 -0400

    commit

--- main.py
+++ main.py
@@ -10,32 +10,45 @@ def encoder(digits):
         new_digits = new_digits + new_dig
     return new_digits
 
-if __name__ == "__main__":
+def decoder(digits):
+    new_digits = ''
+    new_dig = ''
+    for i in range(0, len(digits)):
+        if int(digits[i]) <= 2:
+            new_dig = str(int(digits[i]) + 7)
+        else:
+            new_dig = str(int(digits[i]) - 3)
+        new_digits = new_digits + new_dig
+    return new_digits
+
+def main():
     menu = True
     while menu:
-        password = 0
+        password = ''
         print('Menu')
         print('-------------')
         print('1. Encode')
         print('2. Decode')
         print('3. Quit')
-        print('\n')
+        print()
         option = int(input('Please enter an option: '))
         if option == 1:
             password = input("Please enter your password to encode: ")
             if len(password) != 8:
                 print('Please enter an 8 digit password.')
             if len(password) == 8:
-                password = int(password)
-                # run the encoder function: new_pass = encoder(password)
-                # have a variable like encoded_pass exist
-                print("Your password has been encoded an stored!")
+                encoded_password = encoder(password)
+                print("Your password has been encoded an stored!\n")
         elif option == 2:
-            if password == 0:
+            if len(encoded_password) == 8:
+                decoded_password = decoder(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
+            else:
                 print("Please encode a password first.")
-            # use a decode function to print the statement or just use the print statement
-            # print(f"The encoded password is {encoded_pass}, and the original password is {password}.")
         elif option == 3:
             menu = False
         else:
             print("Please enter a valid option.")
+
+if __name__ == "__main__":
+    main()
