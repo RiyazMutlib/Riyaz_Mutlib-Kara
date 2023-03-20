10c2da7d93dc80ed2d0a2dcc23845800dd36aa26
Riyaz_Mutlib <mutlibriyaz@gmail.com>
2023-03-20 16:35:03 -0400

    comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-def encoder(digits): #subtracts 3 or adds 7 to each digit of the input and returns an encoded password
+def encode(digits): #subtracts 3 or adds 7 to each digit of the input and returns an encoded password
     new_digits = ''
     new_dig = ''
     for i in range(0, len(digits)):
@@ -10,7 +10,7 @@ def encoder(digits): #subtracts 3 or adds 7 to each digit of the input and retur
         new_digits = new_digits + new_dig
     return new_digits
 
-def decoder(digits): #subtracts 3 or adds 7 to each digit of the input and returns the decoded password
+def decode(digits): #subtracts 3 or adds 7 to each digit of the input and returns the decoded password
     new_digits = ''
     new_dig = ''
     for i in range(0, len(digits)):
@@ -37,11 +37,11 @@ def main():
             if len(password) != 8:
                 print('Please enter an 8 digit password.')
             if len(password) == 8:
-                encoded_password = encoder(password)
+                encoded_password = encode(password)
                 print("Your password has been encoded an stored!\n")
         elif option == 2: #decodes password and prints encoded and original password
             if len(encoded_password) == 8:
-                decoded_password = decoder(encoded_password)
+                decoded_password = decode(encoded_password)
                 print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
             else:
                 print("Please encode a password first.")
